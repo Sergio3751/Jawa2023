c09ec055d5ea85d3a8198856fbb911b51663bd52
Samsung <sergiynatali@gmail.com>
2023-11-21 23:28:33 +0100

    1 commit

---
+++ lesson1/main.js
@@ -0,0 +1,47 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let a1='hello'
+console.log(a1);
+let a2='owu';
+console.log(a2);
+let a3='com';
+console.log(a3);
+let a4='UA';
+console.log(a4);
+let a5=1;
+console.log(a5);
+let a6=10;
+console.log(a6);
+let a7=-999;
+console.log(a7);
+let a8=123;
+console.log(a8);
+let PI=3.14;
+console.log(PI);
+let a9=2.7;
+console.log(a9);
+let a10=16;
+console.log(a10);
+let bool=true;
+console.log(bool);
+let bool1=false;
+console.log(bool1);
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName ='Kinash';
+let middleName='Serhii';
+let lastName='Igorovych';
+let person=(firstName+' '+middleName+' '+lastName);
+console.log(person);
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримат
